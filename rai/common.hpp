417254fb7bf87154fab6ed770f83d1e8117bc1cf
clemahieu <clemahieu@gmail.com>
2018-04-01 13:35:30 -0500

    Removing unused function and fix formatting.

--- rai/common.hpp
+++ rai/common.hpp
@@ -21,13 +21,10 @@ struct hash<rai::uint256_union>
 	}
 };
 }
-struct MDB_val;
 namespace rai
 {
 const uint8_t protocol_version = 0x0b;
 const uint8_t protocol_version_min = 0x07;
-
-class block_store;
 
 /**
  * A key pair. The private key is generated from the random pool, or passed in
@@ -41,8 +38,6 @@ public:
 	rai::public_key pub;
 	rai::raw_key prv;
 };
-
-std::unique_ptr<rai::block> deserialize_block (MDB_val const &);
 
 /**
  * Latest information about an account
